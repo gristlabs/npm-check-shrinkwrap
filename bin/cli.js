ef1a4f38016237c65b640e414ec885bc957c6fb7
Dmitry S <dsagal+git@gmail.com>
2017-03-22 16:16:04 -0400

    Initial implementation of npm-check-shrinkwrap
    Test Plan: Tested manually, unittest coming soon
    
    Reviewers: jakestreb
    
    Reviewed By: jakestreb
    
    Subscribers: ysagal
    
    Differential Revision: https://phab.getgrist.com/D847

---
+++ bin/cli.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+"use strict";
+
+const commander = require('commander');
+const main = require('../lib/main.js');
+
+commander
+.usage("[options]")
+.description(
+  `Quickly check if contents of node_modules corresponds to npm-shrinkwrap.json.
+  With --install flag, it can automatically run npm install too.`
+)
+.option("-v, --all", "Print all modules, not only the problem ones", false)
+.option("--no-unwanted", "Ignore node_modules not listed in npm-shrinkwrap.json", false)
+.option("--install", "Automatically run 'npm install --no-save' for all missing modules", false)
+.option("--no-color", "Suppress color output even when the output is to a TTY", false)
+.parse(process.argv);
+
+main(commander)
+.then(success => {
+  process.exit(success ? 0 : 1);
+})
+.catch(err => {
+  console.warn(err.message);
+  process.exit(1);
+});
